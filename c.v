a344c82f5eb422082f3e00bc1a560681031627a8
Abdullo18 <rabdubae@purdue.edu>
2025-02-27 18:07:52 -0500

    new

--- c.v
+++ c.v
@@ -1,8 +1,9 @@
 module counter ( input clk, input rst,
-output reg (31:0] count
+    output reg (31:0] count
 always @(posedge clk)
 begin
-if (rst) begin count <= 0; end else begin
+    if (rst) begin count <= 0; end else begin
 count <= count + 1;
 end end
 endmodule
+//new comment in c
